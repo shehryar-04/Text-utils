f8567ded9b2d6dfcf6d76cb505ecd01df7b86792
shehryar-04 <140516690+shehryar-04@users.noreply.github.com>
2024-05-05 16:50:47 +0500

    Add files via upload

---
+++ mod.mjs
@@ -0,0 +1,5 @@
+import name from "./mod2.mjs"
+import {age} from "./mod2.mjs"
+ 
+console.log(age)
+console.log(name)
---
+++ mod2.mjs
@@ -0,0 +1,5 @@
+const name = "shehryar"
+let age = 20;
+
+export default name;
+export {age}
